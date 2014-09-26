cdcf983e553dd640d5d85c735792c781235a6ae4
Michael Giddens <mikegiddens@silverbiology.com>
2014-09-26 12:05:43 -0600

    Patch
    Fixed issue with uri encoding the ocr

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var gbifNameFinder = function( ocrTxt ) {
+var gbifNameFinder = function (ocrTxt) {
 
 	var needle = require('needle');
 	var route = "http://ecat-dev.gbif.org/ws/indexer";
@@ -6,54 +6,78 @@ var gbifNameFinder = function( ocrTxt ) {
 	this.ocr = ocrTxt || null;
 	this.lastResults = null;
 
-	this.setOcr = function( ocrTxt ) {
+	this.setOcr = function (ocrTxt) {
 		me.ocr = ocrTxt;
 	}
 
-	this.getOcr = function( row ) {
-		if ( row && me.ocr[row] ) return me.ocr.split("\n")[row];
+	this.getOcr = function (row) {
+		if (row && me.ocr[row])
+			return me.ocr.split("\n")[row];
 		return me.ocr;
 	}
 
 	// uses taxon finder service
-	this.findNamesTF = function( callback ) {
-		var input = encodeURIComponent( this.ocr );
+	this.findNamesTF = function (callback) {
+		var input = encodeURIComponent(this.ocr);
 		var type = "text";
 		var format = "json";
 		var req = route + "?input=" + input + "&type=" + type + "&format=" + format;
-		needle.get(req, function(error, response, body){
-			if (response.statusCode == 200) {
-				if (callback) callback( body.names );
-			} else {
-				// error
+		needle.get(req, function (error, response, body) {
+			if (error) {
+				// console.log('Error:', error.stack);
+				// console.log('Req:', req);
+				// console.log('Res:', response);
+				// process.exit(0);
 			}
-		});	
+			if (response.statusCode == 200) {
+				if (callback)
+					callback(body.names);
+			} else {
+				// console.log('No Error....');
+				// console.log('Req:', req);
+				// console.log('Res:', response);
+				// process.exit(0);
+			}
+		});
 	}
-	
+
 	// Uses lucene name finder service
-	this.findNames = function( callback ) {
-		var input = encodeURIComponent( this.ocr );
+	this.findNames = function (callback) {
+		var input = (this.ocr) ? this.ocr.replace(/\r\n/g, '<br>') : '';
+		input = encodeURIComponent(input);
 		var type = "text";
 		var format = "json";
 		var req = route + "?input=" + input + "&type=" + type + "&format=" + format;
-		needle.get(req, function(error, response, body){
-			if (response.statusCode == 200) {
-				me.lastResults = body.names;
-				if (callback) callback( body.names );
-			} else {
-				// error
-			}
-		}, this);	
+		if (input && input !== '') {
+			needle.get(req, function (err, response, body) {
+				if (err) {
+					console.log('--->', err.stack);
+				} else {
+					//console.log("Input:", input)
+					console.log('Body:', body);
+				}
+				if (response && response.statusCode == 200) {
+					me.lastResults = body.names;
+					if (callback)
+						callback(body.names);
+				} else {
+					if (callback)
+						callback([]);
+				}
+			}, this);
+		} else {
+			callback([])
+		}
 	}
-	
+
 	// Returns just an array of scientific names
-	this.getNames = function( limit ) {
+	this.getNames = function (limit) {
 		var tmpList = [];
 		if (this.lastResults) {
 			// Use limit else return all
 			var cnt = limit || this.lastResults.length;
-			for(var i=0; i<cnt; i++) {
-				tmpList.push( this.lastResults[i].scientificName );
+			for (var i = 0; i < cnt; i++) {
+				tmpList.push(this.lastResults[i].scientificName);
 			}
 		}
 		return tmpList;
